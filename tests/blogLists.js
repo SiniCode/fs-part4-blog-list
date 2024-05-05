5912163e485cc5c371261c94f5c8b24045393eae
Sini Arkko <83418050+SiniCode@users.noreply.github.com>
2024-05-05 17:06:40 +0300

    Return accidentally deleted file

---
+++ tests/blogLists.js
@@ -0,0 +1,86 @@
+const listWithOneBlog = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+    }
+]
+
+const blogs = [
+    {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0
+    },
+    {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id: "5a422b3a1b54a676234d17f9",
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+        __v: 0
+    },
+    {
+        _id: "5a422b891b54a676234d17fa",
+        title: "First class tests",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html",
+        likes: 10,
+        __v: 0
+    },
+    {
+        _id: "5a422ba71b54a676234d17fb",
+        title: "TDD harms architecture",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+        likes: 0,
+        __v: 0
+    },
+    {
+        _id: "5a422bc61b54a676234d17fc",
+        title: "Type wars",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+        likes: 2,
+        __v: 0
+    }  
+]
+
+const users = [
+    {
+        _id: "66336a98885106fc2fd41ca0",
+        username: "Don",
+        name: "Donald Duck",
+        passwordHash: "$2b$10$7a2kqE3DhCj09zt3sqwxi.B3Ce3j3Wqw11oGEmA599ZUF0KeAnfYK",
+        blogs:[],
+        __v: 0
+    },
+    {
+        _id: "6633ba7d57a4cd4b5b4cee7a",
+        username: "pooh",
+        name: "Winnie Pooh",
+        passwordHash: "$2b$10$Ob7AKh0L2EJOWCaACgDlmeFSHTc4Vr/S.d/HkhYH2HCLsSpR4dH9C",
+        blogs:[],
+        __v: 0
+    }
+]
+
+
+module.exports = {
+    listWithOneBlog,
+    blogs
+}
